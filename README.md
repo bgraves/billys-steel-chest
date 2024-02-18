d0170430be744033296dbccebaa5cc7dca66cb37
bgraves <38325729+bgraves@users.noreply.github.com>
2024-02-18 18:51:40 -0500

    Add remnants

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # modern-steel-chest
 Factorio mod to replace steel chest graphics with one that looks more like the logistic chests
+
+Graphics copied from AAI Containers mod
--- data.lua
+++ data.lua
@@ -1,8 +1,10 @@
 local itemname = "steel-chest"
 local graphics = "__modern-steel-chest__/graphics/"
-local item = data.raw.item[itemname]
-local entity = data.raw.container[itemname]
+
+local item = data.raw["item"][itemname]
 item.icon = graphics.."icon.png"
+
+local entity = data.raw["container"][itemname]
 entity.icon = graphics.."icon.png"
 entity.circuit_wire_connection_point = {
     wire = {
@@ -20,14 +22,12 @@ entity.picture = {layers = {
         priority = "extra-high",
         width = 32,
         height = 37,
-        frame_count = 7,
         shift = util.by_pixel(0, -2),
         hr_version = {
             filename = graphics.."hr-entity.png",
             priority = "extra-high",
             width = 64,
             height = 74,
-            frame_count = 7,
             shift = util.by_pixel(0, -2),
             scale = 0.5
         }
@@ -37,7 +37,6 @@ entity.picture = {layers = {
         priority = "extra-high",
         width = 51,
         height = 25,
-        repeat_count = 7,
         shift = util.by_pixel(10, 4),
         draw_as_shadow = true,
         hr_version = {
@@ -45,10 +44,54 @@ entity.picture = {layers = {
             priority = "extra-high",
             width = 102,
             height = 50,
-            repeat_count = 7,
             shift = util.by_pixel(10, 4),
             draw_as_shadow = true,
             scale = 0.5
         }
     }
+}}
+
+local remnants = data.raw["corpse"][itemname.."-remnants"]
+remnants.icon = graphics.."icon.png"
+remnants.animation = {layers = {
+    {
+        filename = graphics.."remnants.png",
+        line_length = 1,
+        width = 32,
+        height = 37,
+        frame_count = 1,
+        direction_count = 1,
+        shift = util.by_pixel(0, -2),
+        hr_version = {
+            filename = graphics.."hr-remnants.png",
+            line_length = 1,
+            width = 64,
+            height = 74,
+            frame_count = 1,
+            direction_count = 1,
+            shift = util.by_pixel(0, -2),
+            scale = 0.5
+        }
+    },
+    {
+        filename = graphics.."shadow.png",
+        draw_as_shadow = true,
+        line_length = 1,
+        width = 51,
+        height = 25,
+        frame_count = 1,
+        direction_count = 1,
+        shift = util.by_pixel(10, 4),
+        hr_version = {
+            filename = graphics.."hr-shadow.png",
+            draw_as_shadow = true,
+            line_length = 1,
+            width = 102,
+            height = 50,
+            frame_count = 1,
+            direction_count = 1,
+            shift = util.by_pixel(10, 4),
+            scale = 0.5
+        }
+    },
 }}
