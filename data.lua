743619e51c968697e61b4031bdb7683feede1404
bgraves <38325729+bgraves@users.noreply.github.com>
2024-02-18 21:59:49 -0500

    Use homemade graphics

--- data.lua
+++ data.lua
@@ -6,27 +6,17 @@ item.icon = graphics.."icon.png"
 
 local entity = data.raw["container"][itemname]
 entity.icon = graphics.."icon.png"
-entity.circuit_wire_connection_point = {
-    wire = {
-        green = util.by_pixel(13, -11),
-        red = util.by_pixel(13, -14)
-    },
-    shadow = {
-        green = util.by_pixel(31, 4),
-        red = util.by_pixel(31, 1)
-    }
-}
 entity.picture = {layers = {
     {
         filename = graphics.."entity.png",
         priority = "extra-high",
-        width = 32,
-        height = 37,
+        width = 34,
+        height = 38,
         shift = util.by_pixel(0, -2),
         hr_version = {
             filename = graphics.."hr-entity.png",
             priority = "extra-high",
-            width = 64,
+            width = 66,
             height = 74,
             shift = util.by_pixel(0, -2),
             scale = 0.5
@@ -35,16 +25,18 @@ entity.picture = {layers = {
     {
         filename = graphics.."shadow.png",
         priority = "extra-high",
-        width = 51,
-        height = 25,
-        shift = util.by_pixel(10, 4),
+        width = 56,
+        height = 24,
+        repeat_count = 7,
+        shift = util.by_pixel(12, 5),
         draw_as_shadow = true,
         hr_version = {
             filename = graphics.."hr-shadow.png",
             priority = "extra-high",
-            width = 102,
-            height = 50,
-            shift = util.by_pixel(10, 4),
+            width = 112,
+            height = 46,
+            repeat_count = 7,
+            shift = util.by_pixel(12, 4.5),
             draw_as_shadow = true,
             scale = 0.5
         }
@@ -53,45 +45,22 @@ entity.picture = {layers = {
 
 local remnants = data.raw["corpse"][itemname.."-remnants"]
 remnants.icon = graphics.."icon.png"
-remnants.animation = {layers = {
-    {
-        filename = graphics.."remnants.png",
+remnants.animation = {
+    filename = graphics.."remnants.png",
+    line_length = 1,
+    width = 60,
+    height = 42,
+    frame_count = 1,
+    direction_count = 1,
+    shift = util.by_pixel(10.5, -2.5),
+    hr_version = {
+        filename = graphics.."hr-remnants.png",
         line_length = 1,
-        width = 32,
-        height = 37,
+        width = 116,
+        height = 82,
         frame_count = 1,
         direction_count = 1,
-        shift = util.by_pixel(0, -2),
-        hr_version = {
-            filename = graphics.."hr-remnants.png",
-            line_length = 1,
-            width = 64,
-            height = 74,
-            frame_count = 1,
-            direction_count = 1,
-            shift = util.by_pixel(0, -2),
-            scale = 0.5
-        }
-    },
-    {
-        filename = graphics.."shadow.png",
-        draw_as_shadow = true,
-        line_length = 1,
-        width = 51,
-        height = 25,
-        frame_count = 1,
-        direction_count = 1,
-        shift = util.by_pixel(10, 4),
-        hr_version = {
-            filename = graphics.."hr-shadow.png",
-            draw_as_shadow = true,
-            line_length = 1,
-            width = 102,
-            height = 50,
-            frame_count = 1,
-            direction_count = 1,
-            shift = util.by_pixel(10, 4),
-            scale = 0.5
-        }
-    },
-}}
+        shift = util.by_pixel(10, -3),
+        scale = 0.5
+    }
+}
