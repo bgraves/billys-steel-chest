02d18bb8450b7d1a3c40ca06a89c2ca3b83e9d1e
bgraves <38325729+bgraves@users.noreply.github.com>
2024-10-31 18:03:21 -0400

    Replace normal graphics with HR graphics for 2.0

--- data.lua
+++ data.lua
@@ -10,36 +10,20 @@ entity.picture = {layers = {
     {
         filename = graphics.."entity.png",
         priority = "extra-high",
-        width = 34,
-        height = 38,
+        width = 66,
+        height = 74,
         shift = util.by_pixel(0, -2),
-        hr_version = {
-            filename = graphics.."hr-entity.png",
-            priority = "extra-high",
-            width = 66,
-            height = 74,
-            shift = util.by_pixel(0, -2),
-            scale = 0.5
-        }
+        scale = 0.5
     },
     {
         filename = graphics.."shadow.png",
         priority = "extra-high",
-        width = 56,
-        height = 24,
+        width = 112,
+        height = 46,
         repeat_count = 7,
-        shift = util.by_pixel(12, 5),
+        shift = util.by_pixel(12, 4.5),
         draw_as_shadow = true,
-        hr_version = {
-            filename = graphics.."hr-shadow.png",
-            priority = "extra-high",
-            width = 112,
-            height = 46,
-            repeat_count = 7,
-            shift = util.by_pixel(12, 4.5),
-            draw_as_shadow = true,
-            scale = 0.5
-        }
+        scale = 0.5
     }
 }}
 
@@ -48,19 +32,10 @@ remnants.icon = graphics.."icon.png"
 remnants.animation = {
     filename = graphics.."remnants.png",
     line_length = 1,
-    width = 60,
-    height = 42,
+    width = 116,
+    height = 82,
     frame_count = 1,
     direction_count = 1,
-    shift = util.by_pixel(10.5, -2.5),
-    hr_version = {
-        filename = graphics.."hr-remnants.png",
-        line_length = 1,
-        width = 116,
-        height = 82,
-        frame_count = 1,
-        direction_count = 1,
-        shift = util.by_pixel(10, -3),
-        scale = 0.5
-    }
+    shift = util.by_pixel(10, -3),
+    scale = 0.5
 }
